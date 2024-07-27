b0dfb2878aac469b54987497b5c7c1506e751647
Cody Gima <gima27@up.edu>
2024-07-26 15:43:33 -1000

    fixed home

---
+++ src/components/sub_components/VCHPC.tsx
@@ -0,0 +1,20 @@
+import React from "react";
+
+export default function VCHPC() {
+  return (
+    <>
+      <div className="rounded-box h-auto w-1/2 p-4 m-4 bg-[#eeeeef]">
+        <h2 className="text-2xl text-primary m-2">
+          Vanguard Center for High Performance Computing
+        </h2>
+        <p className="text-black text-xl">
+          MHPCC is the Vanguard of DOD High Performance Computing. MHPCC is
+          exploring emerging technology, with new supercomputing architectures
+          such as IBM POWER8+ and new HPC application areas such as Machine
+          Learning all while striving to lower barriers to high productivity
+          computing.
+        </p>
+      </div>
+    </>
+  );
+}
---
+++ src/components/sub_components/Vision.tsx
@@ -0,0 +1,18 @@
+import React from "react";
+
+export default function Vision() {
+  return (
+    <>
+      <div className="rounded-box h-auto w-1/2 p-4 m-4 bg-[#eeeeef]">
+        <h2 className="text-2xl text-primary m-2">Our Vision</h2>
+        <p className="text-black text-xl">
+          A cadre of experts immersed in a culture of innovation and
+          collaboration, working together as HPCMP pathfinders to explore,
+          evaluate, and apply emerging HPC technology, develop HPC-backed
+          solutions for non-traditional users, and enhance user productivity
+          through a modern HPC ecosystem
+        </p>
+      </div>
+    </>
+  );
+}
