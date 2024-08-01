9b3cc659f9cfb2da290acaaeb6f61e345f6a39a5
joshMori-Pt2 <meowmeow712800@gmail.com>
2024-07-31 17:04:11 -1000

    the end

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,14 +1,10 @@
 import React from "react";
-import AFRL-logo from './AFRL-logo.jpg'
-import ARL_logo from './'
 
 export default function Footer() {
     return (
         <div id="footer">   
             <div id="logos">
-                <a>
-                    import from 
-                </a>
+          
                 <div id="footer-disclaimer">
                     <p>
                     The appearance of hyperlinks does not constitute endorsement by the U.S. Air Force of the linked website or the information, products, or services contained therein. For other than authorized activities such as military exchanges and morale, welfare and recreation sites, the U.S. Air Force does not exercise any editorial control over the information you may find at these locations. Such links are provided consistent with the stated purpose of this DoD web site.
