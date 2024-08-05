e4286d370d09ed454c7e7dc0e851dc8ee7c5ad2e
Cody Gima <gima27@up.edu>
2024-08-05 09:27:59 -1000

    updated footer

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,58 +1,37 @@
 import React from "react";
 
 export default function Header() {
+  // Array of links to path directory
+  const links = [
+    { href: "/about", label: "About Us" },
+    { href: "/whats_new", label: "News & Events" },
+    { href: "/hardware", label: "Hardware" },
+    { href: "/software", label: "Software" },
+    { href: "/user", label: "User Services" },
+    { href: "/doc", label: "Documentation" },
+    { href: "/links", label: "Quick Links" },
+  ];
+
+  // The styling we want for each anchor tag using Tailwind CSS
+  const linkClasses =
+    "transition duration-700 ease-in-out transform hover:scale-125 text-lg hover:text-base-100";
+
   return (
     <header className="flex flex-row flex-wrap justify-center p-4 rounded-btn sticky top-0 z-10">
       <a href=".">
         <img
-          className="h-32 w-auto transition duration-500 ease-in-out transform hover:drop-shadow-[0_0_15px_rgba(227,210,141,0.9)] "
+          className="h-32 w-auto transition duration-500 ease-in-out transform hover:drop-shadow-[0_0_15px_rgba(227,210,141,0.9)]"
           src="/src/img/test_bg1.png"
           alt="MHPCC Logo"
         />
       </a>
       <ul className="flex flex-wrap justify-center items-center ml-10 gap-10 text-default">
-        <a
-          className="transition duration-700 ease-in-out transform hover:scale-125 text-lg hover:text-base-100"
-          href="/about"
-        >
-          About Us
-        </a>
-        <a
-          className="transition duration-700 ease-in-out transform hover:scale-125 text-lg hover:text-base-100"
-          href="/whats_new"
-        >
-          News & Events
-        </a>
-        <a
-          className="transition duration-700 ease-in-out transform hover:scale-125 text-lg hover:text-base-100"
-          href="/hardware"
-        >
-          Hardware
-        </a>
-        <a
-          className="transition duration-700 ease-in-out transform hover:scale-125 text-lg hover:text-base-100"
-          href="/software"
-        >
-          Software
-        </a>
-        <a
-          className="transition duration-700 ease-in-out transform hover:scale-125 text-lg hover:text-base-100"
-          href="/user"
-        >
-          User Services
-        </a>
-        <a
-          className="transition duration-700 ease-in-out transform hover:scale-125 text-lg hover:text-base-100"
-          href="/doc"
-        >
-          Documentation
-        </a>
-        <a
-          className="transition duration-700 ease-in-out transform hover:scale-125 text-lg hover:text-base-100"
-          href="/links"
-        >
-          Quick Links
-        </a>
+        {/* Goes through each index of the links array and references/styles the anchor tag accordingly */}
+        {links.map((link) => (
+          <a key={link.href} className={linkClasses} href={link.href}>
+            {link.label}
+          </a>
+        ))}
       </ul>
     </header>
   );
