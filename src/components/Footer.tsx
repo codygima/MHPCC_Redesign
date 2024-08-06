2d89701dcb632e5c346c123b5f7c02e93192b2c4
Cody Gima <gima27@up.edu>
2024-08-06 10:53:58 -1000

    updated footer

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -2,21 +2,35 @@ import React from "react";
 
 export default function Footer() {
   return (
-    <div id="footer">
+    <footer
+      id="footer"
+      className="pt-10 px-8 pb-2 border-t border-dotted border-t-base-100 w-full text-left text-sm"
+    >
       <div id="logos">
-        <a>import from</a>
         <div id="footer-disclaimer">
-          <p>
-            The appearance of hyperlinks does not constitute endorsement by the
-            U.S. Air Force of the linked website or the information, products,
-            or services contained therein. For other than authorized activities
-            such as military exchanges and morale, welfare and recreation sites,
-            the U.S. Air Force does not exercise any editorial control over the
+          <p className="text-secondary">
+            <span className="font-bold">DISCLAIMER: </span>The appearance of
+            hyperlinks does not constitute endorsement by the U.S. Air Force of
+            the linked website or the information, products, or services
+            contained therein. For other than authorized activities such as
+            military exchanges and morale, welfare and recreation sites, the
+            U.S. Air Force does not exercise any editorial control over the
             information you may find at these locations. Such links are provided
-            consistent with the stated purpose of this DoD web site.
+            consistent with the stated purpose of this DoD web.
+            <span className="text-warning transition duration-700 ease-in-out transform font-thin hover:text-base-100">
+              <a href="https://www.mhpcc.hpc.mil/privacy.html">
+                {" "}
+                Privacy & Security Notice
+              </a>
+            </span>
           </p>
         </div>
       </div>
-    </div>
+    </footer>
   );
 }
+<style>
+  padding-top:50px; border-top: 3px dotted #bbb; position: sticky; left: 0;
+  bottom: 0; width: 100%; background-color: "#f3f4f6"; color: white; text-align:
+  left;
+</style>;
--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,6 +1,6 @@
 import React, { useState } from "react";
 
-export default function Header() {
+export default function MainHeader() {
   // Array of links to path directory
   const links = [
     { href: "/about", label: "About Us" },
@@ -14,14 +14,14 @@ export default function Header() {
 
   // The styling we want for different elements using Tailwind CSS
   const linkClasses =
-    "transition duration-700 ease-in-out transform font-thin hover:scale-125 text-lg hover:text-base-100";
+    "transition duration-700 ease-in-out text-secondary transform font-thin hover:scale-125 text-lg hover:text-base-100";
   const baseNavClass =
-    "flex flex-row flex-wrap justify-center w-full p-4 sticky top-0 z-10 transition duration-500 ease-in-out transform";
+    "flex flex-row flex-wrap items-center justify-between w-full p-4 sticky top-0 z-10 transition duration-500 ease-in-out transform xl:gap-8";
 
   // Logic for Navigation Bar animation
   const [nav, setNavbar] = useState(false);
   const changeBG = () => {
-    if (window.scrollY >= 128) {
+    if (window.scrollY >= 530) {
       setNavbar(true);
     } else {
       setNavbar(false);
@@ -34,22 +34,56 @@ export default function Header() {
     <>
       <nav
         className={`${baseNavClass} ${
-          nav ? "bg-neutral bg-opacity-50 " : "bg-transparent"
+          nav
+            ? "bg-info bg-opacity-90 scale-100"
+            : "xl:bg-transparent xl:scale-90 bg-info bg-opacity-90"
         }`}
       >
-        <a href=".">
-          <img
-            className="h-32 w-auto transition duration-500 ease-in-out transform hover:drop-shadow-[0_0_15px_rgba(227,210,141,0.9)]"
-            src="/src/img/test_bg1.png"
-            alt="MHPCC Logo"
-          />
-        </a>
-        <ul className="flex flex-wrap justify-center items-center ml-10 gap-10 text-default">
-          {/* Goes through each index of the links array and references/styles the anchor tag accordingly */}
+        <div className="flex min-h-[inherit] items-center">
+          <a href=".">
+            <img
+              className="h-32 w-auto transition duration-500 ease-in-out transform hover:drop-shadow-[0_0_15px_rgba(227,210,141,0.9)]"
+              src="/src/img/test_bg1.png"
+              alt="MHPCC Logo"
+            />
+          </a>
+        </div>
+        <label className="btn btn-ghost swap swap-rotate text-lg peer xl:hidden px-2">
+          {/* this hidden checkbox controls the state */}
+          <input type="checkbox" />
+
+          {/* hamburger icon */}
+          <svg
+            className="swap-off fill-current"
+            xmlns="http://www.w3.org/2000/svg"
+            width="32"
+            height="32"
+            viewBox="0 0 512 512"
+          >
+            <path d="M64,384H448V341.33H64Zm0-106.67H448V234.67H64ZM64,128v42.67H448V128Z" />
+          </svg>
+
+          {/* close icon */}
+          <svg
+            className="swap-on fill-current"
+            xmlns="http://www.w3.org/2000/svg"
+            width="32"
+            height="32"
+            viewBox="0 0 512 512"
+          >
+            <polygon points="400 145.49 366.51 112 256 222.51 145.49 112 112 145.49 222.51 256 112 366.51 145.49 400 256 289.49 366.51 400 400 366.51 289.49 256 400 145.49" />
+          </svg>
+        </label>
+        <ul
+          id="nav-items"
+          className="xl:w-fit w-full xl:*:w-fit *:w-full hidden xl:flex flex-wrap items-center gap-3 pb-4 xl:pb-0 *:p-2 peer-has-[:checked]:flex text-center"
+        >
           {links.map((link) => (
-            <a key={link.href} className={linkClasses} href={link.href}>
-              {link.label}
-            </a>
+            <li className={linkClasses}>
+              <a key={link.href} href={link.href}>
+                {link.label}
+              </a>
+            </li>
           ))}
         </ul>
       </nav>
